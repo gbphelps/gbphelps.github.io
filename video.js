0e7fd7b03c9f9700671851c5ec8d2e90d241fa83
Grant Phelps <gbphelps2@gmail.com>
2021-09-06 23:49:56 -0400

    better

--- video.js
+++ video.js
@@ -1,42 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // const videos = Array.from(document.getElementsByTagName('video'));
-    // videos.forEach(vid => {
-    //     vid.addEventListener('click', () => {
-    //         videos.forEach(vid => vid.pause());
-    //         vid.play()
-    //     });
-       
-    // })
-
-
 
     const features = Array.from(document.getElementsByClassName('feature')).filter(f => 
         !Array.from(f.classList).includes('no-video')
     )
+    
     features.forEach(f => {
-        const play = f.getElementsByClassName('play')[0];
         const video = f.getElementsByTagName('video')[0];
-        const pause = f.getElementsByClassName('pause')[0];
         video.loop = true;
 
         video.addEventListener('mouseenter',()=>{
             features.forEach(f => {
-                const play = f.getElementsByClassName('play')[0];
                 const video = f.getElementsByTagName('video')[0];
-                const pause = f.getElementsByClassName('pause')[0];
-                pause.classList.add('hidden');
-                play.classList.remove('hidden');
                 video.pause();
             })
             video.play();
-            pause.classList.remove('hidden');
-            play.classList.add('hidden');
-        })
-
-        pause.addEventListener('click',()=>{
-            video.pause();
-            play.classList.remove('hidden');
-            pause.classList.add('hidden');
         })
     })
 })
